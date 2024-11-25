d8a04704592f03ab3273adbcc595019686d1e341
João Paulo Gomes Rodrigues <joao_pg_rodrigues@estudante.sesisenai.org.br>
2024-11-25 20:55:21 -0300

    .

---
+++ desafioIago/usuario.js
@@ -0,0 +1,27 @@
+const Sequelize = require("sequelize");
+const database = require("./db");
+
+const usuario = database.define(
+   "tb_usuarios",
+   {
+      id: {
+         type: Sequelize.INTEGER,
+         autoIncrement: true,
+         allowNull: false,
+         primaryKey: true,
+      },
+      nome: {
+         type: Sequelize.STRING,
+         allowNull: false,
+      },
+      data_criacao: {
+         type: Sequelize.DATE,
+         allowNull: false,
+      },
+   },
+   {
+      timestamps: false,
+   }
+);
+
+module.exports = usuario;
--- questionsIago.js
+++ questionsIago.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const axios = require("axios");
 
-const Sequelize = require("sequelize");
-
 const app = new express();
 
 const PORT = 3005;
